f6b834f8f27fd8532c165eb71adf50a511d96d14
Word20131124 <miron.mir2913@gmail.com>
2024-11-28 15:09:38 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+let name  = prompt('ismingizni kiriting', 'Mirka');
+
+let surname = prompt('Familiangizni kiriting', 'Maratjanov');
+
+let age = prompt('yoshingizni kiriting', '11');
+
+let subject = prompt('Yoktirgan faningizni kiriting', 'IT');
+
+let street = prompt('Yashaygigan tumaningizni kiriting', 'Mirzo Ulugbek');
+
+let thanks = prompt('malumotlarga kiritgangizni uchun tashakkur');
+
+
+console.log(`ism: ${name}`);
+
+console.log(`familya: ${surname}`);
+
+console.log(`yosh: ${age}`);
+
+console.log(`fan: ${subject}`);
+
+console.log(`tuman: ${street}`);
+
+console.log (thanks);
+
+const namee = prompt('ismingizni kiriting', 'Mirka');
+
+const surnamee = prompt('Familiangizni kiriting', 'Maratjanov');
+
+const agee = prompt('yoshingizni kiriting', '11');
+
+const subjectt = prompt('Yoktirgan faningizni kiriting', 'IT');
+
+const streett = prompt('Yashaygigan tumaningizni kiriting', 'Mirzo Ulugbek');
+
+const thankss = prompt('malumotlarga kiritgangizni uchun tashakkur')
+
+
+console.log(`ism: ${namee}`)
+
+console.log(`familya: ${surnamee}`)
+
+console.log(`yosh: ${agee}`)
+
+console.log(`fan: ${subjectt}`)
+
+console.log(`tuman: ${streett}`)
+
+console.log(thankss)
+
+
+const nameee = prompt('ismingizni kiriting', 'Mirka');
+
+const surnameee = prompt('Familiangizni kiriting', 'Maratjanov');
+
+const ageee = prompt('yoshingizni kiriting', '11');
+
+const subjecttt = prompt('Yoktirgan faningizni kiriting', 'IT');
+
+const streettt = prompt('Yashaygigan tumaningizni kiriting', 'Mirzo Ulugbek');
+
+const thanksss = prompt('malumotlarga kiritgangizni uchun tashakkur')
+
+
+console.log(`ism: ${nameee}`)
+
+console.log(`familya: ${surnameee}`)
+
+console.log(`yosh: ${ageee}`)
+
+console.log(`fan: ${subjecttt}`)
+
+console.log(`tuman: ${streettt}`)
+
+console.log(thanksss)
